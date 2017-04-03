336794608df22920da968474569ed32dadcac2fd
St John Giddy <stjohngiddy@gmail.com>
2017-04-03 14:02:23 +0200

    chore(format): Fix line indent format

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.use(compression())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'DELETE'],
-    preflightContinue: false
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE'],
+  preflightContinue: false
 }))
 
 app.use((req, res, next) => oidc.init(req, res, next))
@@ -46,61 +46,61 @@ app.use((req, res, next) => oidc.init(req, res, next))
  * Login
  */
 app.get('/login', function (req, res) {
-    res.redirect(req.anvil.authorizationUri())
+  res.redirect(req.anvil.authorizationUri())
 })
 
 /**
  * Callback
  */
 app.get('/login/callback', function (req, res) {
-    // Handle OIDC auth callback
+  // Handle OIDC auth callback
 })
 
 /**
  * Unsubscribe
  */
 app.get('/api/v1/student', function (req, res) {
-    console.log("Got request")
-    if (req.query && req.query.id) {
-        // Set up LDAP client
-        var client = ldap.createClient({
-            url: 'ldap://ldap.ru.ac.za'
-        });
-
-        // On connection error
-        client.on('err', function (err) {
-            console.log(err)
-        })
-        // Set up LDAP options (filter, scope)
-        var opts = {
-            filter: `(cn=${req.query.id})`,
-            scope: 'sub'
-        };
-
-        // Search for student number
-        client.search('ou=STUDENT,o=RU', opts, function (err, result) {
-            let results = []
-            // Callback for each result
-            result.on('searchEntry', function (entry) {
-                results.push(entry.object)
-                console.log('entry: ' + JSON.stringify(entry.object));
-            });
-            // Callback for the end of the results
-            result.on('end', function (result) {
-                console.log('status: ' + result.status);
-                res.status(200).json(results)
-            });
-            result.on('error', function (error) {
-                console.log(`error: ${error}`)
-                res.status(404).json(error) // DEBUG: remove .json for prod
-            })
-        });
-    }
+console.log("Got request")
+if (req.query && req.query.id) {
+  // Set up LDAP client
+  var client = ldap.createClient({
+      url: 'ldap://ldap.ru.ac.za'
+  });
+  // On connection error
+  client.on('err', function (err) {
+      console.log(err)
+  })
+  // Set up LDAP options (filter, scope)
+  var opts = {
+      filter: `(cn=${req.query.id})`,
+      scope: 'sub'
+  };
+  // Search for student number
+  client.search('ou=STUDENT,o=RU', opts, function (err, result) {
+      let results = []
+        // Callback for each result
+      result.on('searchEntry', function (entry) {
+        results.push(entry.object)
+        console.log('entry: ' + JSON.stringify(entry.object));
+      });
+        // Callback for the end of the results
+      result.on('end', function (result) {
+        console.log('status: ' + result.status);
+        res.status(200).json(results)
+        client.unbind(); //  Close the connection
+      });
+      result.on('error', function (error) {
+        console.log(`error: ${error}`)
+        res.status(404).json(error) // DEBUG: remove .json for prod
+        client.unbind(); // Close the connection
+      })
+    });
+  }
 })
 
 /**
  * Listen
  */
 app.listen(process.env.PORT || 2456, () => {
-    console.log(`Listening on port ${process.env.PORT || 2456}`)
+  console.log(`Listening on port ${process.env.PORT || 2456}`)
 })
