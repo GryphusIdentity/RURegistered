484fd41eed2b53d26c708e733ab2338bdadf39d5
St John Giddy <stjohngiddy@gmail.com>
2017-04-03 13:40:27 +0200

    refactor(UserToolbar): Modularize the UserToolbar

---
+++ app/js/components/UserToolbar.jsx
@@ -0,0 +1,50 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
+import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar'
+import { connect } from 'react-redux'
+
+const styles = {
+  userToolbarStyle: {
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    flex: 100,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {}
+}
+
+
+const mapStateToProps = (state, ownProps) => {
+  return {}
+}
+
+
+class UserToolbar extends React.Component {
+  render() {
+        let { userToolbarStyle } = styles
+    return (
+      <Toolbar>
+        <ToolbarGroup style={userToolbarStyle}>
+          <ToolbarTitle text="User Actions" />
+          <FlatButton label="Item 1" />
+          <FlatButton label="Item 2" />
+          <FlatButton label="Logout" />
+        </ToolbarGroup>
+      </Toolbar>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserToolbar)
